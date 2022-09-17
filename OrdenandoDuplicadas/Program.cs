5f6de3be4070f7853ce377271bf68c731b85d970
Murilo gabriel nunes <murilognunes12@gmail.com>
2022-09-16 23:54:52 -0300

    Adicionar arquivos de projeto.

---
+++ OrdenandoDuplicadas/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ordenando
+{
+    public class Resolucao
+    {
+        static void Main(string[] args)
+        {
+            string[] letra = {
+                "abracadabra", "allottee",
+                "assessee","kelless", "keenness" };
+
+            for (int i = 0; i < letra.Length; i++)
+            {
+                Console.WriteLine(value: RemoverDuplicata(letra[i]));
+            }
+        }
+        static string RemoverDuplicata(string nome)
+        {
+            char duplicata = ' ';
+
+            string certo = "";
+
+            foreach (char letra in nome)
+            {
+                if (duplicata != letra)
+                {
+                    duplicata = letra;
+                    certo += letra;
+                }
+            }
+
+            return certo;
+        }
+    }
+}
